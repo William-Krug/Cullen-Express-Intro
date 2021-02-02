e3716a71a3200dc5b8f75f7306214ffe5476cfe8
William Krug <william.p.krug@gmail.com>
2021-02-02 15:48:44 -0600

    express linking

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const port = 5000;
 
+// INCANTATION:
+// Share any files inside the "./server/public" folder
+app.use(express.static('server/public'));
+
 // Listen for network requests
 // by convention, use 3000 or 5000 as the port number
 // in theory, can be any port number

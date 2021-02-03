1921dac76207fb20c28b0e403c4a8c4f52a62ed4
William Krug <william.p.krug@gmail.com>
2021-02-03 14:58:15 -0600

    sourcing in jQuery

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,15 @@
 console.log('in client.js');
+
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('so ready');
+}
+
+// Grab that data from the server
+// GET /allTheQuotes
+$.ajax();
+
+// Take array of quotes
+// loop d' lop through em
+// and render (.append()) to the DOM

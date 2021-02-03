7b9cd6e888feec62225a8ce2582648576e7f153e
William Krug <william.p.krug@gmail.com>
2021-02-03 15:53:18 -0600

    .catch()

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,12 +4,48 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('so ready');
+
+  // Grab that data from the server
+  // GET /allTheQuotes
+
+  // Use Ajax!
+  // What is AJAX?
+  // "Asynchronous JavaScript and XML"
+  // Tool for making HTTP network requests
+  // from client-side JavaScript code
+  // ajax is a function that comes with jQuery
+  let ajaxOptions = {
+    url: '/allTheQuotes',
+    method: 'GET',
+  };
+  $.ajax(ajaxOptions)
+    // Promise to call me back later, plz
+    .then(function (quoteList) {
+      console.log('got a response:', quoteList);
+      $('#quoteList').empty();
+      // Take array of quotes
+      // loop d' lop through em
+      for (let quote of quoteList) {
+        // and render (.append()) to the DOM
+        $('#quoteList').append(`
+          <li>
+            <blockquote>"${quote.quote}" —${quote.author}</blockquote>
+          </li>
+        `);
+      }
+    })
+    .catch(function () {
+      // alert('ruh roh... Better contact your webmaster.');
+      $('#messages').text('ruh roh... Better contact your webmaster.');
+    });
+
+  // could also:
+  // $.ajax({
+  //   url: '/allTheQuotes',
+  //   method: 'GET',
+  // }).then(function (response) {
+  //   console.log('got a response:', response);
+  // });
+
+  console.log('so much to do, so little time....');
 }
-
-// Grab that data from the server
-// GET /allTheQuotes
-$.ajax();
-
-// Take array of quotes
-// loop d' lop through em
-// and render (.append()) to the DOM

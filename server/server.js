afb06a12314a79579ec4c9be9a0657e497db18a7
William Krug <william.p.krug@gmail.com>
2021-02-03 09:28:37 -0600

    crated quotes.js module

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 // console.log('express', express);
 
+/// deprecated, express covers this these days
+// const bodyParser = require('body-parser');
+
+const quotes = require('./modules/quotes');
+
 // Create our app(lication)
 const app = express();
 
@@ -9,6 +14,12 @@ const port = 5000;
 // INCANTATION:
 // Share any files inside the "./server/public" folder
 app.use(express.static('server/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for url requests used with our APIs
+
+/// OPTIONAL BODY PARSER USAGE
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 // Listen for network requests
 // by convention, use 3000 or 5000 as the port number
@@ -18,7 +29,18 @@ app.listen(port, function () {
   console.log(`I'm listening....`, port);
 });
 
+// setting up a new route
+// you MUST end API with 'send' or 'end'
+app.get('/quotes', function (req, res) {
+  console.log('GET Request for quotes');
+  res.send(quotes.getNextQuote()); //
+});
+
 // If you see a EADDRINUSE error,
 // run:
 //    killall -9 node
 // ..and try again
+
+// have things separated out into different modules for security, debugging and ease of coding
+// body-parser is middlewear: it takes some input, applies business logic, transforms it and spits it out
+// ..to be used with the rest of your code
